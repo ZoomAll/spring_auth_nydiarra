0f563842513a0432562aeb8bbc35ccb9d1195114
alex <pistoletik@gmail.com>
2017-11-27 07:48:36 +0300

    .

--- src/main/java/ru/dwfe/auth_jwt/AppController.java
+++ src/main/java/ru/dwfe/auth_jwt/AppController.java
@@ -1,24 +1,33 @@
 package ru.dwfe.auth_jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 public class AppController
 {
     private final TokenStore tokenStore;
 
+    private final AccessTokenConverter accessTokenConverter;
+
     @Autowired
-    public AppController(TokenStore tokenStore)
+    public AppController(TokenStore tokenStore, AccessTokenConverter accessTokenConverter)
     {
         this.tokenStore = tokenStore;
+        this.accessTokenConverter = accessTokenConverter;
     }
 
     @RequestMapping("/cities")
@@ -45,15 +54,24 @@ public class AppController
     }
 
     @RequestMapping("/lo")
-    public String lo(OAuth2Authentication auth)
+    public String lo(OAuth2Authentication auth) throws IOException
     {
         if (auth == null)
             return "user is not logged";
 
         OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) auth.getDetails();
         String tokenValue = details.getTokenValue();
-        OAuth2AccessToken oAuth2AccessToken = tokenStore.readAccessToken(tokenValue);
-        tokenStore.removeAccessToken(oAuth2AccessToken);
+
+        JsonParser parser = JsonParserFactory.getJsonParser();
+        Map<String, ?> claims = parser.parseMap(JwtHelper.decode(tokenValue).getClaims());
+
+        String customField = (String) claims.get("user_name");
+
+        System.out.println(claims);
+
+        //OAuth2AccessToken oAuth2AccessToken = tokenStore.readAccessToken(tokenValue);
+        //tokenStore.removeAccessToken(oAuth2AccessToken);
+        //User principal = (User) ((OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication()).getUserAuthentication().getPrincipal();
 
         return "token revoke status = done";
     }
